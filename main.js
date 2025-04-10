bc08ed85b5ae641907a83ef0fa142e9546767f1e
IamCosima <uzairsarvan@gmail.com>
2025-04-10 13:47:46 +0200

    Grid is somewhat created

---
+++ main.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 257; i++) {
+    //var column  = document.createElement("div");
+    var row  = document.createElement("div");
+    row.className = "row";
+    document.getElementById("container").appendChild(row);  
+}
+
+
